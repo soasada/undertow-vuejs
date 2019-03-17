46ae62d695ac331a46e1ffcadc9f7deb063f596f
Nicolas Vargas <nvortega92@gmail.com>
2019-03-17 20:48:29 +0100

    Refactored loadKeyStore method

--- backend/src/main/java/com/popokis/web_app_demo/Application.java
+++ backend/src/main/java/com/popokis/web_app_demo/Application.java
@@ -11,73 +11,73 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
+import java.net.URISyntaxException;
 import java.nio.file.Paths;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 public final class Application {
 
   private static final char[] STORE_PASSWORD = "password".toCharArray();
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws URISyntaxException {
     Undertow.builder()
         .addHttpListener(8080, "localhost")
         .addHttpsListener(8443, "localhost", createSSLContext(loadKeyStore("certificate/client.jks"), loadKeyStore("certificate/clienttrust.jks")))
         .setHandler(Handlers.path()
-            // REST API path
+                // REST API path
 //            .addPrefixPath("/api", Handlers.routing()
 //                .get("/customers", exchange -> {...})
 //                .delete("/customers/{customerId}", exchange -> {...})
 //                .setFallbackHandler(exchange -> {...}))
 
-            // Redirect root path to /static to serve the index.html by default
-            .addExactPath("/about", Handlers.redirect("/"))
+                // Redirect root path to /static to serve the index.html by default
+                .addExactPath("/about", Handlers.redirect("/"))
 
-            // Serve all static files from a folder
-            .addPrefixPath("/", new ResourceHandler(
-                new PathResourceManager(
-                    Paths.get(Application.class.getResource("/public").toURI()),
-                    100))
-                .setWelcomeFiles("index.html")
-                .setDirectoryListingEnabled(true))
+                // Serve all static files from a folder
+                .addPrefixPath("/", new ResourceHandler(
+                    new PathResourceManager(
+                        Paths.get(Application.class.getResource("/public").toURI()),
+                        100))
+                    .setDirectoryListingEnabled(true))
         ).build().start();
   }
 
-  private static KeyStore loadKeyStore(String name) throws Exception {
-    String storeLoc = System.getProperty(name);
-    final InputStream stream;
-    if(storeLoc == null) {
-      stream = Application.class.getResourceAsStream(File.separator + name);
-    } else {
-      stream = Files.newInputStream(Paths.get(storeLoc));
-    }
-
-    if(stream == null) {
-      throw new RuntimeException("Could not load keystore");
-    }
-    try(InputStream is = stream) {
+  private static KeyStore loadKeyStore(String name) {
+    try (InputStream is = Application.class.getResourceAsStream(File.separator + name)) {
       KeyStore loadedKeystore = KeyStore.getInstance("JKS");
       loadedKeystore.load(is, STORE_PASSWORD);
       return loadedKeystore;
+    } catch (IOException | CertificateException | NoSuchAlgorithmException | KeyStoreException e) {
+      throw new RuntimeException(e);
     }
   }
 
-  private static SSLContext createSSLContext(final KeyStore keyStore, final KeyStore trustStore) throws Exception {
-    KeyManager[] keyManagers;
-    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-    keyManagerFactory.init(keyStore, STORE_PASSWORD);
-    keyManagers = keyManagerFactory.getKeyManagers();
+  private static SSLContext createSSLContext(final KeyStore keyStore, final KeyStore trustStore) {
+    try {
+      KeyManager[] keyManagers;
+      KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      keyManagerFactory.init(keyStore, STORE_PASSWORD);
+      keyManagers = keyManagerFactory.getKeyManagers();
 
-    TrustManager[] trustManagers;
-    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-    trustManagerFactory.init(trustStore);
-    trustManagers = trustManagerFactory.getTrustManagers();
+      TrustManager[] trustManagers;
+      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      trustManagerFactory.init(trustStore);
+      trustManagers = trustManagerFactory.getTrustManagers();
 
-    SSLContext sslContext;
-    sslContext = SSLContext.getInstance("TLSv1.3");
-    sslContext.init(keyManagers, trustManagers, null);
+      SSLContext sslContext;
+      sslContext = SSLContext.getInstance("TLSv1.3");
+      sslContext.init(keyManagers, trustManagers, null);
 
-    return sslContext;
+      return sslContext;
+    } catch (UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
