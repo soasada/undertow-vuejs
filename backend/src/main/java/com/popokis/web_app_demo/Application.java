386a50d7e3033c1ad5189d7c5e5cce92ff42854a
Nicolas Vargas <nvortega92@gmail.com>
2019-03-17 19:50:43 +0100

    Serving all static files with Undertow

--- backend/src/main/java/com/popokis/web_app_demo/Application.java
+++ backend/src/main/java/com/popokis/web_app_demo/Application.java
@@ -1,9 +1,33 @@
 package com.popokis.web_app_demo;
 
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.PathResourceManager;
+import io.undertow.server.handlers.resource.ResourceHandler;
+
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 public final class Application {
 
-  public static void main(String[] args) {
-    SimpleServer simpleServer = new SimpleServer(8080, "localhost", new V1Router());
-    simpleServer.start();
+  public static void main(String[] args) throws URISyntaxException {
+    Undertow.builder().addHttpListener(8080, "localhost")
+        .setHandler(Handlers.path()
+            // REST API path
+//            .addPrefixPath("/api", Handlers.routing()
+//                .get("/customers", exchange -> {...})
+//                .delete("/customers/{customerId}", exchange -> {...})
+//                .setFallbackHandler(exchange -> {...}))
+
+            // Redirect root path to /static to serve the index.html by default
+//            .addExactPath("/", Handlers.redirect("/static"))
+
+            // Serve all static files from a folder
+            .addPrefixPath("/", new ResourceHandler(
+                new PathResourceManager(
+                    Paths.get(Application.class.getResource("/public").toURI()),
+                    100))
+                .setWelcomeFiles("index.html"))
+        ).build().start();
   }
 }
--- backend/src/main/java/com/popokis/web_app_demo/Router.java
+++
@@ -1,9 +0,0 @@
-package com.popokis.web_app_demo;
-
-import java.util.List;
-
-public interface Router {
-  List<Route> routes();
-
-  String version();
-}
