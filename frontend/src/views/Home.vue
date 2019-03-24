6466404d545c98b1b60237b7619f61ea16835b97
Nicolas Vargas <nvortega92@gmail.com>
2019-03-24 12:33:24 +0100

    Added object mapping dependency

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
+    export default {
   name: 'home',
   components: {
     HelloWorld
