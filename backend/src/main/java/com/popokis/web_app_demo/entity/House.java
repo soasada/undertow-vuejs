58679111469df35851571c4b678b96c8349fb658
Nicolas Vargas <nvortega92@gmail.com>
2019-03-27 21:47:54 +0100

    Added house fields

--- backend/src/main/java/com/popokis/web_app_demo/entity/House.java
+++ backend/src/main/java/com/popokis/web_app_demo/entity/House.java
@@ -1,4 +1,40 @@
 package com.popokis.web_app_demo.entity;
 
-public class House {
+import java.time.LocalDateTime;
+
+public final class House {
+
+  private final Long id;
+  private final String name;
+  private final Long userId;
+  private final LocalDateTime createdAt;
+  private final LocalDateTime updatedAt;
+
+  public House(Long id, String name, Long userId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.id = id;
+    this.name = name;
+    this.userId = userId;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Long getUserId() {
+    return userId;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
 }
