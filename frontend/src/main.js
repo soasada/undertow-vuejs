195782858fbd688c782fc4a8f2ee1acb73fe650d
Nicolas Vargas <nvortega92@gmail.com>
2019-04-27 10:51:17 +0200

    Updated dependencies and changed bootstrap for bulma

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bulma/css/bulma.min.css';
 
 Vue.config.productionTip = false;
 
