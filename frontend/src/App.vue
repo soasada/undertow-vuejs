86d3382490e2f3f886ab7478fd8da04cadc42a6e
Nicolas Vargas <nvortega92@gmail.com>
2019-04-27 10:58:28 +0200

    Changing navbar to bulma

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,26 @@
 <template>
     <div id="app">
-        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 class="my-0 mr-md-auto font-weight-normal">Demo</h5>
-            <nav class="my-2 my-md-0 mr-md-3">
+        <nav class="level">
+            <p class="level-item has-text-centered">
                 <router-link class="p-2 text-dark" active-class="active" to="/" exact>Home</router-link>
+            </p>
+            <p class="level-item has-text-centered">
                 <router-link class="p-2 text-dark" active-class="active" to="/about">About</router-link>
+            </p>
+            <p class="level-item has-text-centered">
+                <img src="https://bulma.io/images/bulma-type.png" alt="" style="height: 30px;">
+            </p>
+            <p class="level-item has-text-centered">
                 <router-link class="p-2 text-dark" active-class="active" to="/users">Users</router-link>
-            </nav>
-            <a class="btn btn-outline-primary" href="#">Sign up</a>
-        </div>
+            </p>
+            <p class="level-item has-text-centered">
+                <a class="link is-info">Contact</a>
+            </p>
+        </nav>
 
         <router-view/>
 
-        <footer class="container">
+        <footer class="footer">
             <p>© {{ new Date().getFullYear() }} Popokis.com ·
                 <router-link to="/about">About</router-link>
                 ·
