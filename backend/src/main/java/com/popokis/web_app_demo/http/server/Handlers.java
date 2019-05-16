7f440343a60ef21eb022c231d2a9d67fb56640cf
Nicolas Vargas <nvortega92@gmail.com>
2019-05-16 20:40:43 +0200

    Added Request and Response handlers as a proof of concept, not using yet

--- backend/src/main/java/com/popokis/web_app_demo/http/server/Handlers.java
+++ backend/src/main/java/com/popokis/web_app_demo/http/server/Handlers.java
@@ -1,76 +1,57 @@
 package com.popokis.web_app_demo.http.server;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.async.NonBlockingInputFeeder;
-import com.google.common.base.Stopwatch;
 import com.popokis.web_app_demo.exception.Exceptions;
 import com.popokis.web_app_demo.mapper.json.JsonMapper;
 import com.popokis.web_app_demo.mapper.json.JsonMappers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.BlockingHandler;
 import io.undertow.util.PathTemplateMatch;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 public final class Handlers {
 
   private Handlers() {}
 
-  public static <R, S> HttpHandler bodyBased(Class<R> requestType, Function<R, S> f, Logger logger) {
+  public static <R, S> HttpHandler bodyBased(Class<R> requestType, Function<R, S> f) {
     return new BlockingHandler(
         exchange -> {
           try {
-            JsonFactory jsonFactory = new JsonFactory();
-            JsonParser parser = jsonFactory.createNonBlockingByteArrayParser();
-            NonBlockingInputFeeder feeder = parser.getNonBlockingInputFeeder();
             String jsonBody = Requests.body(exchange);
             R request = JsonMappers.model(jsonBody, requestType);
             S response = f.apply(request);
-            String jsonResponse = JsonMapper.getInstance().toJson(response);
-            Responses.asJson(exchange, jsonResponse);
+            Responses.asJson(exchange, JsonMapper.getInstance().toJson(response));
           } catch (Exception e) {
-            long elapsedTime = requestStopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
-            Responses.asJson(exchange, Exceptions.rootCause(e).getMessage(), logger, elapsedTime);
+            Responses.asJson(exchange, Exceptions.rootCause(e).getMessage());
           }
         }
     );
   }
 
-  public static <S> HttpHandler idBased(Function<Long, S> f, Logger logger) {
+  public static <S> HttpHandler idBased(Function<Long, S> f) {
     return new BlockingHandler(
         exchange -> {
-          Stopwatch requestStopwatch = Stopwatch.createStarted();
           try {
             PathTemplateMatch pathMatch = exchange.getAttachment(PathTemplateMatch.ATTACHMENT_KEY);
             long id = Long.parseLong(pathMatch.getParameters().get("id"));
             S response = f.apply(id);
-            long elapsedTime = requestStopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
-            String jsonResponse = JsonMapper.getInstance().toJson(response);
-            Responses.asJson(exchange, jsonResponse, logger, elapsedTime);
+            Responses.asJson(exchange, JsonMapper.getInstance().toJson(response));
           } catch (Exception e) {
-            long elapsedTime = requestStopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
-            Responses.asJson(exchange, Exceptions.rootCause(e).getMessage(), logger, elapsedTime);
+            Responses.asJson(exchange, Exceptions.rootCause(e).getMessage());
           }
         }
     );
   }
 
-  public static <S> HttpHandler listBased(Function<Void, List<S>> f, Logger logger) {
+  public static <S> HttpHandler listBased(Function<Void, List<S>> f) {
     return new BlockingHandler(
         exchange -> {
-          Stopwatch requestStopwatch = Stopwatch.createStarted();
           try {
             List<S> response = f.apply(null);
-            long elapsedTime = requestStopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
-            String jsonResponse = JsonMapper.getInstance().toJson(response);
-            Responses.asJson(exchange, jsonResponse, logger, elapsedTime);
+            Responses.asJson(exchange, JsonMapper.getInstance().toJson(response));
           } catch (Exception e) {
-            long elapsedTime = requestStopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
-            Responses.asJson(exchange, Exceptions.rootCause(e).getMessage(), logger, elapsedTime);
+            Responses.asJson(exchange, Exceptions.rootCause(e).getMessage());
           }
         }
     );
--- backend/src/main/java/com/popokis/web_app_demo/http/server/RequestHandler.java
+++ backend/src/main/java/com/popokis/web_app_demo/http/server/RequestHandler.java
@@ -1,7 +1,10 @@
 package com.popokis.web_app_demo.http.server;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Stopwatch;
 import com.popokis.web_app_demo.exception.Exceptions;
+import com.popokis.web_app_demo.mapper.json.JsonMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.AttachmentKey;
@@ -10,7 +13,11 @@ import io.undertow.util.StatusCodes;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.message.Message;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.UUID;
 
 public final class RequestHandler implements HttpHandler {
@@ -30,9 +37,9 @@ public final class RequestHandler implements HttpHandler {
   @Override
   public void handleRequest(HttpServerExchange exchange) throws Exception {
     stopwatch.start();
+    String correlationId = UUID.randomUUID().toString();
     exchange.getRequestReceiver().receiveFullString((fullExchange, data) -> {
       try {
-        String correlationId = UUID.randomUUID().toString();
         fullExchange.getRequestHeaders().add(new HttpString("X-Correlation-Id"), correlationId);
         ThreadContext.put("requested-url", exchange.getRequestURL());
         ThreadContext.put("message-type", "REQUEST");
@@ -41,12 +48,57 @@ public final class RequestHandler implements HttpHandler {
         fullExchange.putAttachment(REQUEST_PAYLOAD, data);
         next.handleRequest(fullExchange);
       } catch (Exception e) {
-        throw new RuntimeException(e);
+        exceptionTermination(fullExchange, e, correlationId);
       }
-    }, (errorExchange, exception) -> {
-      logger.error(Exceptions.rootCause(exception)); // TODO: enhance and log correctly exceptions
-      errorExchange.setStatusCode(StatusCodes.BAD_REQUEST);
-      errorExchange.getResponseSender().send("");
-    });
+    }, (errorExchange, exception) -> exceptionTermination(errorExchange, exception, correlationId));
+  }
+
+  private void exceptionTermination(HttpServerExchange exchange, Throwable exception, String correlationId) {
+    Writer stackTraceStringWriter = new StringWriter();
+    PrintWriter stackTracePrintWriter = new PrintWriter(stackTraceStringWriter);
+    String exceptionMessage = Exceptions.rootCause(exception).getMessage();
+
+    exception.printStackTrace(stackTracePrintWriter);
+    logger.error(jsonErrorMessage(correlationId, stackTraceStringWriter.toString()));
+
+    exchange.setStatusCode(StatusCodes.BAD_REQUEST);
+    exchange.getResponseSender().send(exceptionMessage);
+  }
+
+  private Message jsonErrorMessage(String correlationId, String exceptionMessage) {
+    JsonNodeFactory jsonNodeFactory = new JsonNodeFactory(false);
+    ObjectNode jsonError = new ObjectNode(jsonNodeFactory);
+    jsonError.set("id", jsonError.textNode(correlationId));
+    jsonError.set("msg", jsonError.textNode(exceptionMessage));
+    return new JsonMessageError(jsonError);
+  }
+
+  private static class JsonMessageError implements Message {
+
+    private final Object object;
+
+    public JsonMessageError(final Object object) {
+      this.object = object;
+    }
+
+    @Override
+    public String getFormattedMessage() {
+      return JsonMapper.getInstance().toJson(object);
+    }
+
+    @Override
+    public String getFormat() {
+      return object.toString();
+    }
+
+    @Override
+    public Object[] getParameters() {
+      return new Object[]{object};
+    }
+
+    @Override
+    public Throwable getThrowable() {
+      return null;
+    }
   }
 }
