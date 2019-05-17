72237bd56f0ed19bdefdb07f7779979cb52046b7
Nicolas Vargas Ortega <nvortega92@gmail.com>
2019-05-17 11:55:11 +0200

    Now the server is listening in 0.0.0.0, and added docker testing

--- prod_reset_db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mysql -uroot -proot <<< "DELETE FROM production_db.furniture; ALTER TABLE production_db.furniture AUTO_INCREMENT = 1;"
-mysql -uroot -proot <<< "DELETE FROM production_db.house; ALTER TABLE production_db.house AUTO_INCREMENT = 1;"
-mysql -uroot -proot <<< "DELETE FROM production_db.user; ALTER TABLE production_db.user AUTO_INCREMENT = 1;"
