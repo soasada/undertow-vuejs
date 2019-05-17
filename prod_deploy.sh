a5d3861474d311bfb2497e7f5e48f7fc7493e709
Nicolas Vargas Ortega <nvortega92@gmail.com>
2019-05-17 11:29:22 +0200

    Added timezone to all db configs

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -4,5 +4,5 @@ mvn -U clean install -pl :frontend
 rm -rf backend/src/main/resources/public
 mysql -uroot -proot <<< "DROP DATABASE production_db; CREATE DATABASE production_db;"
 mvn -U clean -pl :backend flyway:migrate
-export APP_ENV=prod && mvn -U clean compile test package -pl :backend
+export APP_ENV=test && mvn -U clean compile test package -pl :backend
 export APP_ENV=prod && java -Xms64M -Xmx512M -Dlogfilename=web -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar backend/target/backend-0.0.1.jar &
