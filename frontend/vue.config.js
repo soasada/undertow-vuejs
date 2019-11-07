55ce4fb150c351bbf4b176dbeb54096aebde2719
Nicolas Vargas <nvortega92@gmail.com>
2019-11-07 18:20:48 +0100

    Updated vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api/v1': {
-                target: 'http://localhost:8080',
+                target: 'http://localhost:8081',
                 ws: true,
                 changeOrigin: true
             }
