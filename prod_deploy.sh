345b8b0f9426f23067fec463ad1be008dc5fe9e4
Nicolas Vargas <nvortega92@gmail.com>
2019-11-08 16:23:50 +0100

    Updated deploy script

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -3,6 +3,9 @@
 CONTAINER_NAME="$(docker ps --format '{{.Names}} {{.Ports}}' | grep '0.0.0.0:3329->' | awk '{ print $1 }' || true)"
 BACKEND_PID="$(jps -l | grep 'backend/target/backend-0.0.1.jar' | awk '{print $1}')"
 
+echo "Killing PID: $BACKEND_PID"
+kill -9 $BACKEND_PID
+
 mvn -U clean install -pl :frontend
 rm -rf backend/src/main/resources/public
 docker exec -i $CONTAINER_NAME mysql -uroot -proot <<< "DROP DATABASE production_db; CREATE DATABASE production_db;"
@@ -10,10 +13,6 @@ mysql -uroot -proot <<< "DROP DATABASE production_db; CREATE DATABASE production
 mvn -U clean -pl :backend -Dflyway.configFiles=src/main/resources/database/test_migrations.properties flyway:migrate
 mvn -U clean -pl :backend flyway:migrate
 export APP_ENV=test && mvn -U clean compile test package -pl :backend
-
-echo "Killing PID: $BACKEND_PID"
-kill -9 $BACKEND_PID
-
 export APP_ENV=prod && java -Xms64M -Xmx256M -Dlogfilename=web -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar backend/target/backend-0.0.1.jar &
 docker exec -i $CONTAINER_NAME mysql -uroot -proot <<< "DELETE FROM production_db.furniture; ALTER TABLE production_db.furniture AUTO_INCREMENT = 1;"
 docker exec -i $CONTAINER_NAME mysql -uroot -proot <<< "DELETE FROM production_db.house; ALTER TABLE production_db.house AUTO_INCREMENT = 1;"
